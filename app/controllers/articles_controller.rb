3d8b4c7628b04a916b2eba70069f85cd6e56557e
thadeus <thadeusnyariki94@gmail.com>
2022-12-01 21:11:21 +0300

    Done assignment

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,6 @@
 class ArticlesController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
-
+  
   def index
     articles = Article.where(is_member_only: false).includes(:user).order(created_at: :desc)
     render json: articles, each_serializer: ArticleListSerializer
